4b65ab5da2a5a1d76395b8995c75690a40d07838
yiedw <yiedw@naver.com>
2019-09-24 09:17:48 +0900

    db비밀번호 암호화

--- project1/db/user/userController.js
+++ project1/db/user/userController.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
+var crypto=require('crypto');
 // var alert=require('node-popup')
 var alert = require('alert-node')
+
+
 
 
 router.use(bodyparser.urlencoded({ extended: true }));
@@ -23,20 +26,27 @@ var userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    salt:{
+        type:String
     }
 })
 var User = mongoose.model('User', userSchema);
 exports.create = function (req, res) {
 
     User.find({ 'id': req.body.id }, function (err, data) {
+        //중복 확인후 없으면 데이터 추가
         if (err) console.log(err)
         else {
-            //중복 확인후 있으면 데이터 추가
+            //비밀번호 암호화
+            let salt=Math.round((new Date().valueOf()*Math.random()))+"";
+            let hashPassword=crypto.createHash("sha512").update(req.body.password+salt).digest('hex');
             if (data == "") {
                 User.create({
                     Eth_Address: req.body.Eth_Address,
                     id: req.body.id,
-                    password: req.body.password
+                    password: hashPassword,
+                    salt:salt
                 }, function (err, user) {
                     if (err) {
                         return res.status(500).send("User 생성 실패");
@@ -48,8 +58,7 @@ exports.create = function (req, res) {
                 })
             }
             else {
-                
-                alert("이미 등록된 id입니다")
+                // alert("이미 등록된 id입니다")
 
             }
         }
