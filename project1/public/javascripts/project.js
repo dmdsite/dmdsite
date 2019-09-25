f6838a1b22ed280e6aee9ce22b3a84617bfa6463
lyz0238@naver.com <52765639+lyz0238@users.noreply.github.com>
2019-09-25 17:48:24 +0900

    confirm기능

--- project1/public/javascripts/project.js
+++ project1/public/javascripts/project.js
@@ -1,25 +1,30 @@
-$(document).ready(function(){
-    
-   
-      $("#signup").click(function(){
-          location.href = "/add";
-      })
-      $("#signin").click(function(){
-          location.href = "/login";
-      })
-      $("#again").click(function(){
-        location.href = "/add";
-      })
-     
-      
-    $('.form-signin button').click(function () {
+$(document).ready(function () {
+
+
+  $("#signup").click(function () {
+    location.href = "/add";
+  })
+  $("#signin").click(function () {
+    location.href = "/login";
+  })
+  $("#again").click(function () {
+    location.href = "/add";
+  })
+
+
+  $('.form-signin button').click(function () {
+    if ($('#password').val() != $('#confirm_Password').val()) {
+      window.alert('passwords are not matching!!!!!!!!!!!!!!')
+      return;
+    }
+
     $.ajax('/create', {
       'method': 'POST',
       'data': {
         'Eth_Address': $('#inputEthAddress').val(),
         'id': $('#inputId').val(),
         'password': $('#inputPassword').val()
-          }
+      }
     })
     // var Eth_Address = $('#inputEthAddress').val()
     // var id = $('#inputEthAddress').val()
